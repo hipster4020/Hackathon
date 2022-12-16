38eab41ad588c7ebf3711e459b2d320d28915e7f
hipster4020 <hipster4020@gmail.com>
2022-12-16 17:43:01 +0900

    dataloader 추가

--- src/preprocessing/data_merge.py
+++ src/preprocessing/data_merge.py
@@ -25,37 +25,51 @@ def main():
             entries = os.listdir(path)
             for e, ee in enumerate(entries):
                 with open(os.path.join(path, str(ee))) as f:
-                    for j in json.load(f)["sessionInfo"][0]["dialog"]:
-                        j["conv_no"] = e + 1
-                        data[k].append(j)
+                    rawfile = json.load(f)
+                # personaFeatures, prevTimeInfo, dialog:speaker, utterance, summary, prevAggregatedpersonaSummary
+                temp = {}
+                temp["filename"] = rawfile["FileInfo"]["filename"]
+                temp["personaFeatures"] = rawfile["personaInfo"]["clInfo"][
+                    "personaFeatures"
+                ]
+                temp["session"] = [
+                    {
+                        "prevTimeInfo": session["prevTimeInfo"],
+                        "dialog": [
+                            {kk: row[kk] for kk in ["speaker", "utterance", "summary"]}
+                            for row in session["dialog"]
+                        ],
+                        "prevAggregatedpersonaSummary": session[
+                            "prevAggregatedpersonaSummary"
+                        ],
+                    }
+                    for session in rawfile["sessionInfo"]
+                ]
+                data[k].append(temp)
     train_data, val_data, test_data = data
-
-    # json to dataframe
-    df_train = pd.DataFrame(train_data)
-    df_val = pd.DataFrame(val_data)
-    df_test = pd.DataFrame(test_data)
 
     # data save
     s_list = ["train", "val", "test"]
     for s in s_list:
-        path = config.pwd + s + ".json"
+        path = os.path.join(config.pwd + s + ".json")
 
-        temp_dict = [
-            {
-                "speaker": row["speaker"].strip(),
-                "personaID": row["personaID"].strip(),
-                "utterance": row["utterance"].strip(),
-                "summary": row["summary"].strip(),
-                "terminate": row["terminate"].strip(),
-                "conv_no": row["conv_no"],
-            }
-            for _, row in eval("df_" + s).iterrows()
-        ]
+        # temp_dict = [
+        #     {
+        #         "speaker": row["speaker"].strip(),
+        #         "personaID": row["personaID"].strip(),
+        #         "utterance": row["utterance"].strip(),
+        #         "summary": row["summary"].strip(),
+        #         "terminate": row["terminate"].strip(),
+        #         "conv_no": row["conv_no"],
+        #     }
+        #     for _, row in eval("df_" + s).iterrows()
+        # ]
+
         with open(path, "w", encoding="utf-8") as f:
-            for line in temp_dict:
+            for line in eval(f"{s}_data"):
                 json_record = json.dumps(line, ensure_ascii=False)
                 f.write(json_record + "\n")
-        print("Saved {} : {} records".format(eval("df_" + s), len(eval("df_" + s))))
+        print("Saved {} : {} records".format(f"{s}_data", len(f"{s}_data")))
 
 
 if __name__ == "__main__":
